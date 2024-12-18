d433303ab832428c9b69af994bc3d8c6f2acac0f
Daniel Elskamp <ich@dginx.de>
2024-12-18 02:37:48 +0100

    init

---
+++ src/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DGINXReal\Providers;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../../config/laravel-auth.php', 'laravel-auth');
+    }
+
+    public function boot(): void
+    {
+        if($this->app->runningInConsole()){
+            $this->publishes([
+                __DIR__.'/../../resources/views' => resource_path('views/vendor/laravel-auth'),
+                __DIR__.'/../../config/laravel-auth.php' => config_path('laravel-auth.php')
+            ]);
+        }
+
+        //Views
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'laravel-auth');
+
+        //Language Files
+        $this->loadTranslationsFrom(__DIR__ . '/../../lang', 'laravel-auth');
+    }
+}
